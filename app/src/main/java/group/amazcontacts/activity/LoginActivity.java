34513465eb2d1a2c237a62cc264d2be070651ced
quangtmhe130060 <quangtmhe130060@fpt.edu.vn>
2020-03-19 21:25:18 +0700

    QuangTMHe130060 setup for sign up and login

---
+++ app/src/main/java/group/amazcontacts/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package group.amazcontacts.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import group.amazcontacts.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/group/amazcontacts/activity/SignUpActivity.java
@@ -0,0 +1,16 @@
+package group.amazcontacts.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import group.amazcontacts.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
